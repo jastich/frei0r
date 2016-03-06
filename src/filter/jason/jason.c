8ed32eec284f9ba503454382593bfc46a21fdb86
Jason Stich <jason.stich@gmail.com>
2016-03-05 23:26:01 -0600

    Made section blocks that are entirely filled with their average color

--- src/filter/jason/jason.c
+++ src/filter/jason/jason.c
@@ -1,17 +1,22 @@
 #include <stdlib.h>
+#include <string.h>
 #include <assert.h>
+#include <stdio.h>
 
 #include "frei0r.h"
 
-static const unsigned int SIZE = 20;
+typedef unsigned int uint;
+
+static const uint SIZE = 5;
 
 typedef struct inverter_instance
 {
-    unsigned int width;
-    unsigned int height;
-    unsigned int sectionWidth;
-    unsigned int sectionHeight;
-    unsigned int** map;
+    uint width;
+    uint height;
+    uint sectionWidth;
+    uint sectionHeight;
+    uint numSectionsX;
+    uint numSectionsY;
 } plugin_instance_t;
 
 int f0r_init()
@@ -40,13 +45,13 @@ void f0r_get_param_info(f0r_param_info_t* info, int param_index)
     /* no params */
 }
 
-f0r_instance_t f0r_construct(unsigned int width, unsigned int height)
+f0r_instance_t f0r_construct(uint width, uint height)
 {
     plugin_instance_t* inst = (plugin_instance_t*)calloc(1, sizeof(*inst));
     inst->width = width; inst->height = height;
     inst->sectionWidth = SIZE; inst->sectionHeight = SIZE;
-    unsigned int sectionsX = 
-    inst->map = new unsigned int
+    inst->numSectionsX = inst->width / inst->sectionWidth;
+    inst->numSectionsY = inst->height / inst->sectionHeight;
     return (f0r_instance_t)inst;
 }
 
@@ -68,23 +73,54 @@ void f0r_update(f0r_instance_t instance, double time,
 {
     assert(instance);
     plugin_instance_t* inst = (plugin_instance_t*)instance;
-    unsigned int w = inst->width;
-    unsigned int h = inst->height;
-    unsigned int x,y;
+    uint w = inst->width;
+    uint h = inst->height;
+    uint sw = inst->sectionWidth;
+    uint sh = inst->sectionHeight;
+    uint nsx = inst->numSectionsX;
+    uint nsy = inst->numSectionsY;
+    uint sx, sy, i, j;
   
     uint32_t* dst = outframe;
     const uint32_t* src = inframe;
 
-    for (x=0; x<w; x+=sectionWidth)
+    for (sx=0; sx<nsx; sx++)
     {
-        for (y=0; y<h; y+=sectionHeight)
+        for (sy=0; sy<nsy; sy++)
         {
+            // ARGB color model
+            #define R(v) ((v >> 16) & 0xFF)
+            #define G(v) ((v >> 8)  & 0xFF)
+            #define B(v) (v & 0xFF)
+            #define RGB(r, g, b) (r << 16) | (g << 8) | b
+            #define PIXEL_AT(b, x, y) &b[(y) * inst->width + (x)]
+            uint32_t x0 = sx*sw;
+            uint32_t y0 = sy*sh;
+            uint32_t avg = *PIXEL_AT(inframe, x0, y0);
+            uint32_t avgR = R(avg);
+            uint32_t avgG = G(avg);
+            uint32_t avgB = B(avg);
+            uint32_t n = 1;
+            for (i=0; i<sw; i++)
+            {
+                for (j=0; j<sh; j++)
+                {
+                    uint32_t px = *PIXEL_AT(inframe, x0+i, y0+j);
+                    avgR = (avgR * n + R(px)) / (n + 1);
+                    avgG = (avgG * n + G(px)) / (n + 1);
+                    avgB = (avgB * n + B(px)) / (n + 1);
+                    n++;
+                }
+            }
+            for (i=0; i<sw; i++)
+            {
+                for (j=0; j<sh; j++)
+                {
+                    // Iterate each row in the section and memset to average
+                    *PIXEL_AT(outframe, x0+i, y0+j) = RGB(avgR, avgG, avgB);
+                }   
+            }
         }
     }
-    
-
-  for(y=0;y<h;++y)
-      for(x=0;x<w;++x,++src)
-	  *dst++ = 0x00ffffff^(*src); 
 }
 
