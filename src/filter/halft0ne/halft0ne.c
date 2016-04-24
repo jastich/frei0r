bd4b8e1ac76aa9a9cc52b7e47703820c8ec4e3b4
Jason Stich <jason.stich@gmail.com>
2016-04-24 10:20:16 -0500

    Implemented recursive grouping algorithm but has stack overflow issues

--- src/filter/halft0ne/halft0ne.c
+++ src/filter/halft0ne/halft0ne.c
@@ -4,7 +4,6 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <math.h>
-#include <cairo.h>
 #include "frei0r.h"
 
 // ARGB color model
@@ -12,13 +11,40 @@
 #define G(v) (((v) >> 8)  & 0xFF)
 #define B(v) ((v) & 0xFF)
 #define RGB(r, g, b) ((r) << 16) | ((g) << 8) | (b)
-#define PIXEL_AT(b, x, y) &b[(y) * inst->width + (x)]
+#define PIXEL_AT(b, x, y, w) &b[(y) * (w) + (x)]
+#define X(ptr, start, w) (int32_t)((ptr - start) % w)
+#define Y(ptr, start, w) (int32_t)((ptr - start) / w)
 
 typedef unsigned int uint;
 
-static const uint SIZE = 30;
+typedef struct
+{
+    uint x;
+    uint y;
+    uint radius;
+} dot_info;
 
-typedef struct inverter_instance
+static const uint SIZE = 30;
+static const float NUM_COLORS = 255;
+static const uint NUM_BITS_RED = 3;
+static const uint NUM_BITS_GREEN = 3;
+static const uint NUM_BITS_BLUE = 2;
+static const uint MAX_DOT_RADIUS = 15;
+
+typedef struct group_node
+{
+    struct group_node* pNext;
+    struct group_node* pPrev;
+} group_node;
+
+typedef struct
+{
+    uint64_t* pValues;
+    uint64_t* pLastValue;
+    uint32_t capacity;
+} stack_t;
+
+typedef struct
 {
     uint width;
     uint height;
@@ -26,7 +52,55 @@ typedef struct inverter_instance
     uint sectionHeight;
     uint numSectionsX;
     uint numSectionsY;
+    stack_t groupStack;
+    group_node* pGroups;
+    group_node onStack;
 } plugin_instance_t;
+
+void clear(stack_t* pStack)
+{
+    pStack->pLastValue = NULL;
+}
+
+void init(stack_t* pStack, uint32_t capacity)
+{
+    pStack->pValues = calloc(capacity, sizeof(uint64_t));
+    pStack->capacity = capacity;
+    clear(pStack);
+}
+
+int isEmpty(stack_t* pStack)
+{
+    return pStack->pLastValue == NULL;
+}
+
+void push(stack_t* pStack, uint64_t value)
+{
+    if (NULL == pStack->pLastValue)
+    {
+        pStack->pValues[0] = value;
+        pStack->pLastValue = pStack->pValues;
+    }
+    else if ((pStack->pLastValue + 1) < (pStack->pValues + pStack->capacity))
+    {
+        pStack->pLastValue++;
+        *pStack->pLastValue = value;
+    }
+    printf( "Pushed %p, size=%d\n",
+            (void*)value, pStack->pLastValue-pStack->pValues+1 );
+}
+
+uint64_t pop(stack_t* pStack)
+{
+    uint64_t retval = *pStack->pLastValue--;
+    if (pStack->pLastValue < pStack->pValues)
+    {
+        pStack->pLastValue = NULL;
+    }
+    printf("Popped %p, %d items remaining on stack\n", (void*)retval,
+           NULL != pStack->pLastValue ? pStack->pLastValue - pStack->pValues : 0);
+    return retval;
+}
 
 int f0r_init()
 {
@@ -42,7 +116,6 @@ void f0r_get_plugin_info(f0r_plugin_info_t* inverterInfo)
     inverterInfo->author = "Jason Stich";
     inverterInfo->plugin_type = F0R_PLUGIN_TYPE_FILTER;
     inverterInfo->color_model = F0R_COLOR_MODEL_RGBA8888;
-    inverterInfo->frei0r_version = FREI0R_MAJOR_VERSION;
     inverterInfo->major_version = 0; 
     inverterInfo->minor_version = 9; 
     inverterInfo->num_params =  0; 
@@ -59,15 +132,21 @@ f0r_instance_t f0r_construct(uint width, uint height)
 {
     plugin_instance_t* inst = (plugin_instance_t*)calloc(1, sizeof(*inst));
     inst->width = width; inst->height = height;
+    printf( "%dx%d\n", width, height );
     inst->sectionWidth = SIZE; inst->sectionHeight = SIZE;
     inst->numSectionsX = inst->width / inst->sectionWidth;
     inst->numSectionsY = inst->height / inst->sectionHeight;
+    init(&inst->groupStack, width*height);
+    inst->pGroups = (group_node*)calloc(width*height, sizeof(group_node));
     return (f0r_instance_t)inst;
 }
 
 void f0r_destruct(f0r_instance_t instance)
 {
-    free(instance);
+    plugin_instance_t* pInst = (plugin_instance_t*)instance;
+    free(pInst->groupStack.pValues);
+    free(pInst->pGroups);
+    free(pInst);
 }
 
 void f0r_set_param_value(f0r_instance_t instance, 
@@ -105,85 +184,135 @@ void rgb_to_cmyk(uint32_t* rgb, float* cmyk)
     cmyk[Y] = (1.0-rgbPrime[B]-cmyk[K]) / (1.0-cmyk[K]);
 }
 
+/// Shifts color of pixel so that there are at most N colors in entire image.
+uint32_t rebase(uint32_t value, uint32_t oldBase, uint32_t newBase)
+{
+    uint32_t sectorWidth = oldBase / newBase;
+    uint32_t remainder = value % sectorWidth;
+    uint32_t retval = value - remainder;
+    if (remainder > sectorWidth / 2)
+    {
+        retval += remainder + sectorWidth;
+    }
+    return retval;
+}
+ 
+/// Shifts color of each pixel so that there are at most N colors in entire image.
+void flatten(plugin_instance_t* pInst, const uint32_t* pInBuffer, uint32_t* pOutBuffer)
+{
+    uint i, j;
+
+    for (i=0; i<pInst->width; i++)
+    {
+        for (j=0; j<pInst->height; j++)
+        {
+            static const uint NUM_COLORS = 7;
+            const uint32_t* px = PIXEL_AT(pInBuffer, i, j, pInst->width);
+            uint32_t* pxOut = PIXEL_AT(pOutBuffer, i, j, pInst->width);
+            uint32_t oldBase = 0xFF;
+            uint32_t r = rebase(R(*px), oldBase, NUM_COLORS);
+            uint32_t g = rebase(G(*px), oldBase, NUM_COLORS);
+            uint32_t b = rebase(B(*px), oldBase, NUM_COLORS);
+            *pxOut = RGB(r, g, b);
+        }
+    }
+}
+
+char* pBase;
+
+void searchPx(
+    plugin_instance_t* pInst, uint32_t* pFrame,
+    int32_t x, int32_t y, uint32_t color,
+    group_node** ppLast)
+{
+    uint32_t* pCurrPx = PIXEL_AT(pFrame, x, y, pInst->width);
+    printf("Stack: %d\n", ((char*)&pCurrPx)-pBase);
+    group_node* pCurr = pInst->pGroups + (pCurrPx-pFrame);
+    printf( "x=%d y=%d prev=%p next=%p px=0x%x color=0x%x\n",
+            x, y, pCurr->pPrev, pCurr->pNext, *pCurrPx,
+            color );
+    if (color == *pCurrPx)
+    {
+        // If the pixel hasn't already been grouped...
+        if (((NULL == pCurr->pNext) && (NULL == pCurr->pPrev)) ||
+            (&pInst->onStack == pCurr->pPrev))
+        {
+            // Join it to the group of the previous pixel.
+            (*ppLast)->pNext = pCurr;
+            pCurr->pPrev = *ppLast;
+            *ppLast = pCurr;
+            // Search each pixel above, below, left, and right of it.
+            if (x - 1 >= 0)
+            {
+                searchPx(pInst, pFrame, x-1, y, color, ppLast);
+            }
+            if (y - 1 >= 0)
+            {
+                searchPx(pInst, pFrame, x, y-1, color, ppLast);
+            }
+            if (x + 1 < pInst->width)
+            {
+                searchPx(pInst, pFrame, x+1, y, color, ppLast);
+            }
+            if (y + 1 < pInst->height)
+            {
+                searchPx(pInst, pFrame, x, y+1, color, ppLast);
+            }
+        }
+    }
+    else if ((NULL == pCurr->pNext) && (NULL == pCurr->pPrev))
+    {
+        // Push pixel to stack if it isn't a member of a group and doesn't
+        // match the current group.
+        push(&pInst->groupStack, (uint64_t)pCurr);
+        pCurr->pPrev = &pInst->onStack;
+    }
+}
+
 void f0r_update(f0r_instance_t instance, double time,
 		const uint32_t* inframe, uint32_t* outframe)
 {
+    printf("f0r_update\n");
     assert(instance);
-    plugin_instance_t* inst = (plugin_instance_t*)instance;
-    uint w = inst->width;
-    uint h = inst->height;
-    uint sw = inst->sectionWidth;
-    uint sh = inst->sectionHeight;
-    uint nsx = inst->numSectionsX;
-    uint nsy = inst->numSectionsY;
-    uint sx, sy, i, j;
-  
-    uint32_t* dst = outframe;
-    const uint32_t* src = inframe;
-    
-    // TODO explain this assertion
-    assert( ( inst->width * sizeof(uint32_t) ) ==
-            cairo_format_stride_for_width(CAIRO_FORMAT_ARGB32, inst->width) );
-    cairo_surface_t* pSurface = cairo_image_surface_create_for_data(
-        outframe,
-        CAIRO_FORMAT_ARGB32,
-        inst->width,
-        inst->height,
-        inst->width * sizeof(uint32_t));
-    assert(CAIRO_STATUS_SUCCESS == cairo_surface_status(pSurface));
-    // Create Cairo context with surface as target.
-    cairo_t* pCr = cairo_create(pSurface);
-    cairo_set_line_width(pCr, 1);
+    plugin_instance_t* pInst = (plugin_instance_t*)instance;
+    memset(pInst->pGroups, 0, sizeof(group_node)*pInst->width*pInst->height);
+    clear(&(pInst->groupStack));
 
-    // Calculate average of pixel values in evenly spaced sections
-    for (sx=0; sx<nsx; sx++)
+    // Reduce number of colors in the image.
+    flatten(pInst, inframe, outframe);
+
+    int i, j, numzeros=0;
+    for (i=0; i<pInst->width*pInst->height; i++)
     {
-        for (sy=0; sy<nsy; sy++)
+        if (outframe[i] == 0) numzeros++;
+    }
+    printf("numzeros = %d", numzeros);
+
+    pBase = (char*)(&i);
+
+    // Push one pixel to the stack to get the search started.  Pixels will be
+    // pushed and popped from the stack until all pixels are processed.
+    push(&pInst->groupStack, (uint64_t)pInst->pGroups);
+    // Flag the pixel for being on the stack.
+    pInst->pGroups->pPrev = &pInst->onStack;
+
+    while (!isEmpty(&pInst->groupStack))
+    {
+        // Pop next pixel off stack.  Pointer points to last pixel in
+        // current group.
+        group_node* pLast = (group_node*)pop(&pInst->groupStack);
+        // Only process this pixel if it hasn't already been grouped by a
+        // previous search path.
+        if (pLast->pPrev == &pInst->onStack )
         {
-            uint32_t x0 = sx*sw;
-            uint32_t y0 = sy*sh;
-            uint32_t avg = *PIXEL_AT(inframe, x0, y0);
-            uint32_t avgR = R(avg);
-            uint32_t avgG = G(avg);
-            uint32_t avgB = B(avg);
-            uint32_t n = 1;
-            for (i=0; i<sw; i++)
-            {
-                for (j=0; j<sh; j++)
-                {
-                    uint32_t px = *PIXEL_AT(inframe, x0+i, y0+j);
-                    avgR = (avgR * n + R(px)) / (n + 1);
-                    avgG = (avgG * n + G(px)) / (n + 1);
-                    avgB = (avgB * n + B(px)) / (n + 1);
-                    n++;
-                }
-            }
-
-            // Draw a shape in that region.
-            cairo_set_source_rgb(
-                pCr,
-                (float)avgR / 255.0,
-                (float)avgG / 255.0,
-                (float)avgB / 255.0);
-            uint size = (sw/4) * rand() % 40;
-            cairo_translate(pCr, x0, y0);
-            cairo_arc(pCr, 0, 0, size, 0, 2 * M_PI);
-            // cairo_rectangle(pCr, x0, y0, size, size);
-            cairo_stroke_preserve(pCr);
-            cairo_fill(pCr);
-
-            /*
-            // Iterate each pixel in the section and fill to average
-            for (i=0; i<sw; i++)
-            {
-                for (j=0; j<sh; j++)
-                {
-                    *PIXEL_AT(outframe, x0+i, y0+j) = RGB(avgR, avgG, avgB);
-                }   
-            }*/
+            // Clear "on stack" flag now that it has been popped.
+            pLast->pPrev = NULL;
+            int32_t x = X(pLast, pInst->pGroups, pInst->width); 
+            int32_t y = Y(pLast, pInst->pGroups, pInst->width);
+            uint32_t color = *(outframe + (pLast-pInst->pGroups));
+            printf("Index = %d\n", pLast-pInst->pGroups);
+            searchPx(pInst, outframe, x, y, color, &pLast);
         }
     }
-
-    cairo_surface_destroy(pSurface);
 }
 
